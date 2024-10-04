d5ce90ff28b7c7cd888465cae43faba87e5ceb54
Nzola Kiampava <nzolakiampava@gmail.com>
2024-10-04 12:20:54 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -25,21 +25,22 @@ int	check_death(t_data *data)
 	int			i;
 	long long 	cur_time;
 
-	i = 0;
+	i = -1;
 	while (1)
 	{
-		while (i < data->num_philo)
+		while (++i < data->num_philo)
 		{
 			cur_time = get_time_in_ms();
 			pthread_mutex_lock(&data->print_mutex);
 			if (cur_time - data->philo[i].last_meal_time > data->time_to_die)
 			{
-				cur_time = cur_time - data->start_time;
-				printf("%lld %d died\n", cur_time, data->philo[i].id);
+				printf("%lld %d died\n", cur_time - data->start_time, data->philo[i].id);
+				pthread_mutex_unlock(&data->print_mutex);
 				return (1);
 			}
 			pthread_mutex_unlock(&data->print_mutex);
 		}
 		usleep(1000);
 	}
+	return (0);
 }
