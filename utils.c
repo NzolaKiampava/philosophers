dd1ed4744b17f2f6ee2c9f39138592bf47ea52cb
Nzola Kiampava <nzolakiampava@gmail.com>
2024-10-05 16:38:29 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -22,25 +22,27 @@ long long	get_time_in_ms(void)
 
 int	check_death(t_data *data)
 {
-	int			i;
-	long long 	cur_time;
+    int i;
+    struct timeval tv; // Use struct timeval to get microsecond precision
 
-	i = -1;
-	while (1)
-	{
-		while (++i < data->num_philo)
-		{
-			cur_time = get_time_in_ms();
-			pthread_mutex_lock(&data->print_mutex);
-			if (cur_time - data->philo[i].last_meal_time > data->time_to_die)
-			{
-				printf("%lld %d died\n", cur_time - data->start_time, data->philo[i].id);
-				pthread_mutex_unlock(&data->print_mutex);
-				return (1);
-			}
-			pthread_mutex_unlock(&data->print_mutex);
-		}
-		usleep(1000);
-	}
-	return (0);
+    while (1) {
+        i = -1;  // Reset the counter
+        while (++i < data->num_philo) {
+            // Get the current time with microsecond precision
+            gettimeofday(&tv, NULL);
+            long long cur_time_ms = (tv.tv_sec * 1000LL) + (tv.tv_usec / 1000); // in milliseconds
+            long long cur_time_us = tv.tv_usec; // in microseconds
+
+            // Check if the philosopher has exceeded their time to die
+            if (cur_time_ms - data->philo[i].last_meal_time > data->time_to_die) {
+                pthread_mutex_lock(&data->print_mutex);  // Lock for printing
+                // Print the philosopher id and timestamp with microsecond detail
+                printf("%lld.%03lld %d \033[31m died\n\033[0m", cur_time_ms, cur_time_us % 1000, data->philo[i].id);
+                pthread_mutex_unlock(&data->print_mutex);
+                return (1);  // Philosopher died, stop the simulation
+            }
+        }
+        usleep(1000);  // Sleep for 1 ms between checks
+    }
+    return (0);
 }
