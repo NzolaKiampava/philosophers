90594ac648ee5f096f374f9a6fa3467d45222164
Nzola Kiampava <nzolakiampava@gmail.com>
2024-11-08 10:59:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,57 +6,26 @@
 /*   By: nkiampav <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 11:54:38 by nkiampav          #+#    #+#             */
-/*   Updated: 2024/09/30 14:11:04 by nkiampav         ###   ########.fr       */
+/*   Updated: 2024/11/06 09:53:07 by nkiampav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int	parse_arguments(int argc, char **argv, t_data *data)
-{
-	if (argc != 5 && argc != 6)
-	{
-		printf(STR_USAGE, argv[0]);
-		return (1);
-	}
-	data->num_philo = atoi(argv[1]);
-	data->time_to_die = atoi(argv[2]);
-	data->time_to_eat = atoi(argv[3]);
-	data->time_to_sleep = atoi(argv[4]);
-	if (argc == 6)
-		data->must_eat_count = atoi(argv[5]);
-	else
-		data->must_eat_count = -1;
-	return (0);
-}
-
 int	main(int argc, char **argv)
 {
-    t_data data;
+	t_data		data;
+	int			i;
 
-    // Parse command-line arguments
-    if (parse_arguments(argc, argv, &data)) {
-        return (1); // Error in parsing
-    }
-
-    // Initialize mutexes
-    if (init_mutexes(&data) != 0) {
-        return (1); // Error in mutex initialization
-    }
-
-    // Initialize philosophers
-    if (init_philo(&data) != 0) {
-        destroy_mutexes(&data); // Clean up mutexes if initialization fails
-        return (1); // Error in philosopher initialization
-    }
-
-    // Start the simulation
-    start_simulation(&data);
-
-    // Clean up resources after the simulation completes
-    destroy_mutexes(&data); // Ensure all mutexes are destroyed
-    free(data.philo); // Free the philosophers array
-    free(data.forks); // Free the forks array
-
-    return (0); // Successful termination
+	if (parse_args(argc, argv, &data))
+		return (1);
+	if (init_simulation(&data))
+	{
+		printf("Error: Failed to initialize simulation\n");
+		return (1);
+	}
+	for (i = 0; i < data.num_philosophers; i++)
+		pthread_join(data.philosophers[i].thread, NULL);
+	cleanup_simulation(&data);
+	return (0);
 }
