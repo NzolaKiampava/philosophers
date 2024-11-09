550fcee213397e2c5893ce34c764f9d913617a02
Nzola Kiampava <nzolakiampava@gmail.com>
2024-11-09 15:24:15 +0100

    Update monitor.c

--- monitor.c
+++ monitor.c
@@ -12,45 +12,58 @@
 
 #include "philo.h"
 
-int check_if_all_ate(t_data *data)
+int	check_if_all_ate(t_data *data)
 {
-    int i;
+	int	i;
 
-    if (data->must_eat_count < 0)
-        return (0);
-    for (i = 0; i < data->num_philosophers; i++)
-        if (data->philosophers[i].meals_eaten < data->must_eat_count)
-            return (0);
-    return (1);
+	i = -1;
+	if (data->must_eat_count < 0)
+		return (0);
+	while (++i < data->num_philosophers)
+	{
+		if (data->philosophers[i].meals_eaten < data->must_eat_count)
+			return (0);
+	}
+	return (1);
 }
 
-void *death_monitor(void *arg)
+static int	check_philosopher_death(t_data *data, int i)
 {
-    t_data *data = (t_data *)arg;
-    int i;
-    long long current_time;
+	long long	current_time;
+	long long	time_since_meal;
 
-    while (!check_if_all_ate(data))
-    {
-        i = 0;
-        while (i < data->num_philosophers)
-        {
-            current_time = get_time();
-            pthread_mutex_lock(&data->write_lock);
-            if (!data->someone_died &&
-                current_time - data->philosophers[i].last_meal_time > data->time_to_die)
-            {
-                printf("%lld %d died\n", get_time() - data->start_time, i + 1);
-                data->someone_died = 1;
-                pthread_mutex_unlock(&data->write_lock);
-                return (NULL);
-            }
-            pthread_mutex_unlock(&data->write_lock);
-            i++;
-        }
-        if (data->someone_died)
-            break;
-        usleep(1000);
-    }
-    return (NULL);
+	current_time = get_time();
+	pthread_mutex_lock(&data->write_lock);
+	time_since_meal = current_time - data->philosophers[i].last_meal_time;
+	if (!data->someone_died && time_since_meal > data->time_to_die)
+	{
+		printf("%lld %d died\n", get_time() - data->start_time, i + 1);
+		data->someone_died = 1;
+		pthread_mutex_unlock(&data->write_lock);
+		return (1);
+	}
+	pthread_mutex_unlock(&data->write_lock);
+	return (0);
+}
+
+void	*death_monitor(void *arg)
+{
+	t_data		*data;
+	int			i;
+
+	data = (t_data *)arg;
+	while (!check_if_all_ate(data))
+	{
+		i = 0;
+		while (i < data->num_philosophers)
+		{
+			if (check_philosopher_death(data, i))
+				return (NULL);
+			i++;
+		}
+		if (data->someone_died)
+			break ;
+		usleep(1000);
+	}
+	return (NULL);
 }
