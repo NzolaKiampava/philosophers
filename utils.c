c4ad6ad5e8a7cca805ce1da90f99c219def25696
Nzola Kiampava <nzolakiampava@gmail.com>
2024-11-09 15:25:55 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -12,14 +12,14 @@
 
 #include "philo.h"
 
-static int ft_isdigit(int c)
+static int	ft_isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
-        return (c);
-    return (0);
+	if (c >= '0' && c <= '9')
+		return (c);
+	return (0);
 }
 
-static int	ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
 	int	num;
 	int	isneg;
@@ -47,72 +47,22 @@ static int	ft_atoi(const char *str)
 	return (num * isneg);
 }
 
-void print_status(t_data *data, int id, char *status)
+void	print_status(t_data *data, int id, char *status)
 {
-    pthread_mutex_lock(&data->write_lock);
-    if (!data->someone_died)
-        printf("%lld %d %s\n", get_time() - data->start_time, id + 1, status);
-    pthread_mutex_unlock(&data->write_lock);
-}
-
-int is_valid_number(char *str)
-{
-    long long num;
-    int i;
-
-    num = 0;
-    i = 0;
-    if (str[0] == '-')
-        return (0);
-    while (str[i])
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return (0);
-        num = (num * 10) + (str[i] - '0');
-        if (num > INT_MAX)
-            return (0);
-        i++;
-    }
-    return (1);
-}
-
-int parse_args(int argc, char **argv, t_data *data)
-{
-    if (argc != 5 && argc != 6)
-    {
-        printf(STR_USAGE);
-        return (1);
-    }
-    if (!is_valid_number(argv[1]) || !is_valid_number(argv[2]) || 
-        !is_valid_number(argv[3]) || !is_valid_number(argv[4]) || 
-        (argc == 6 && !is_valid_number(argv[5])))
-    {
-        printf("Error: Invalid arguments\n");
-        return (1);
-    }
-    data->num_philosophers = ft_atoi(argv[1]);
-    data->time_to_die = ft_atoi(argv[2]);
-    data->time_to_eat = ft_atoi(argv[3]);
-    data->time_to_sleep = ft_atoi(argv[4]);
-    data->must_eat_count = (argc == 6) ? ft_atoi(argv[5]) : -1;
-    data->someone_died = 0;
-    if (data->num_philosophers <= 0 || data->time_to_die <= 0 || 
-        data->time_to_eat <= 0 || data->time_to_sleep <= 0 || 
-        (data->must_eat_count <= 0 && argc == 6))
-    {
-        printf("Error: Invalid arguments\n");
-        return (1);
-    }
-    return (0);
+	pthread_mutex_lock(&data->write_lock);
+	if (!data->someone_died)
+		printf("%lld %d %s\n", get_time() - data->start_time, id + 1, status);
+	pthread_mutex_unlock(&data->write_lock);
 }
 
 void	cleanup_simulation(t_data *data)
 {
-    int	i;
+	int	i;
 
-    for (i = 0; i < data->num_philosophers; i++)
-        pthread_mutex_destroy(&data->forks[i]);
-    pthread_mutex_destroy(&data->write_lock);
-    free(data->forks);
-    free(data->philosophers);
+	i = -1;
+	while (++i < data->num_philosophers)
+		pthread_mutex_destroy(&data->forks[i]);
+	pthread_mutex_destroy(&data->write_lock);
+	free(data->forks);
+	free(data->philosophers);
 }
