5421f7543a7d68087d9187bb10f4039126ab39bf
Nzola Kiampava <nzolakiampava@gmail.com>
2024-11-23 17:25:53 -0500

    Update init.c

--- init.c
+++ init.c
@@ -12,6 +12,27 @@
 
 #include "philo.h"
 
+int	init_data(t_data *data, int argc, char **argv)
+{
+	data->num_philosophers = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	data->must_eat_count = -1;
+	if (argc == 6)
+		data->must_eat_count = ft_atoi(argv[5]);
+	if (data->num_philosophers < 1 || data->time_to_die < 0
+		|| data->time_to_eat < 0 || data->time_to_sleep < 0
+		|| (argc == 6 && data->must_eat_count < 0))
+		return (1);
+	if (init_mutexes(data) != 0)
+		return (1);
+	if (init_philosophers(data) != 0)
+		return (1);
+	data->start_time = get_time();
+	return (0);
+}
+
 int	init_mutexes(t_data *data)
 {
 	int	i;
@@ -19,14 +40,17 @@ int	init_mutexes(t_data *data)
 	data->forks = malloc(sizeof(pthread_mutex_t) * data->num_philosophers);
 	if (!data->forks)
 		return (1);
-	if (pthread_mutex_init(&data->write_lock, NULL))
-		return (1);
 	i = -1;
 	while (++i < data->num_philosophers)
 	{
-		if (pthread_mutex_init(&data->forks[i], NULL))
+		if (pthread_mutex_init(&data->forks[i], NULL) != 0)
 			return (1);
 	}
+	if (pthread_mutex_init(&data->meal_mutex, NULL) != 0
+		|| pthread_mutex_init(&data->print_mutex, NULL) != 0
+		|| pthread_mutex_init(&data->death_mutex, NULL) != 0)
+		return (1);
+	data->someone_died = 0;
 	return (0);
 }
 
@@ -34,39 +58,18 @@ int	init_philosophers(t_data *data)
 {
 	int	i;
 
-	i = -1;
 	data->philosophers = malloc(sizeof(t_philosopher) * data->num_philosophers);
 	if (!data->philosophers)
 		return (1);
+	i = -1;
 	while (++i < data->num_philosophers)
 	{
-		data->philosophers[i].id = i;
-		data->philosophers[i].left_fork = i;
-		data->philosophers[i].right_fork = (i + 1) % data->num_philosophers;
+		data->philosophers[i].id = i + 1;
 		data->philosophers[i].meals_eaten = 0;
 		data->philosophers[i].last_meal_time = get_time();
+		data->philosophers[i].left_fork = i;
+		data->philosophers[i].right_fork = (i + 1) % data->num_philosophers;
 		data->philosophers[i].data = data;
 	}
-	return (0);
-}
-
-int	init_simulation(t_data *data)
-{
-	int			i;
-	pthread_t	monitor;
-
-	i = -1;
-	if (init_mutexes(data) || init_philosophers(data))
-		return (1);
-	data->start_time = get_time();
-	while (++i < data->num_philosophers)
-	{
-		if (pthread_create(&data->philosophers[i].thread, NULL,
-				philosopher_routine, &data->philosophers[i]))
-			return (1);
-	}
-	if (pthread_create(&monitor, NULL, death_monitor, data))
-		return (1);
-	pthread_detach(monitor);
 	return (0);
 }
